48407b91715d12f7da209432d914eabf479c4987
Brent Bauman <brentbauman86@gmail.com>
2018-08-15 01:17:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   # Add your post route and action below
   post '/food' do
     @name = params[:name]
-    @food = params[:food]
+    @favorite_food = params[:favorite_food]
 
   end
 end
